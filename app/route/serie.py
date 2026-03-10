7a65677e2cc68a1e9b6f170b9ca48b96b673ddad
MathiasAntunes-dev <mathias.a.silva@aluno.senai.br>
2026-03-10 15:52:47 -0300

    build: Código atual sem erro.

---
+++ app/route/serie.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.model.serie import SerieModel
+from app.schema.serie import SerieSchema
+
+serie = APIRouter()
+
+@serie.post("/")
+async def criar_serie(dados: SerieSchema, db: Session = Depends(get_db)):
+    nova_serie = SerieModel(**dados.model_dump())
+    db.add(nova_serie)
+    db.commit()
+    db.refresh(nova_serie)
+    return nova_serie
+
+@serie.get("/series")
+async def listar_series(db: Session = Depends(get_db)):
+    return db.query(SerieModel).all()
+
+# Tarefa 1: Crie as rotas de atualização e deleção da API
+# Tarefa 2: Resolva todos os erros da sua aplicação
+# Tarefa 3: Resolva todos os erros das novas rotas
+# Versione
+
+# Extra: resolva o erro de importação das variáveis de ambiente
+#  detectado no módulo python-dotenv  e utilize corretamente
+#  a importação com a função load_dotenv() em seu database.py
